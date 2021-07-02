71da058c76531ff58b24333e768ef3c42e7d235f
watertreestar <watertreestar@gmail.com>
2021-07-03 00:32:58 +0800

    feat: mini k-v db

---
+++ minidb/internal/dbfile.go
@@ -0,0 +1,82 @@
+package db
+
+import (
+	"os"
+)
+
+type File struct {
+	Path     string
+	File     *os.File
+	Offset   int64
+	FileName string
+}
+
+// AppendEntry  追加Entry到文件
+func (dbFile *File) AppendEntry(entry *Entry) error {
+	enc, err := entry.Encode()
+	if err != nil {
+		return err
+	}
+	_, err = dbFile.File.Write(enc)
+	// Increase current offset to write data
+	dbFile.Offset += entry.GetSize()
+	return nil
+}
+
+// ReadEntry 从file中指定的offset处开始读取
+func (dbFile *File) ReadEntry(offset int64) (*Entry, error) {
+	// 1. Reader entry header first
+	buf := make([]byte, EntryHeaderSize)
+	if _, err := dbFile.File.ReadAt(buf, offset); err != nil {
+		return nil, err
+	}
+	e, err := Decode(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	dbFile.increaseOffset(int64(EntryHeaderSize))
+
+	// Extract key size and value size from entry header
+	// 2. Read key from entry file
+	if e.KeySize > 0 {
+		key := make([]byte, e.KeySize)
+		if _, err = dbFile.File.ReadAt(key, dbFile.Offset); err != nil {
+			return nil, err
+		}
+		e.Key = key
+	}
+
+	dbFile.increaseOffset(int64(e.KeySize))
+	// 3. Read value from entry file
+	if e.ValueSize > 0 {
+		value := make([]byte, e.ValueSize)
+		if _, err = dbFile.File.ReadAt(value, dbFile.Offset); err != nil {
+			return nil, err
+		}
+		e.Value = value
+	}
+
+	return e, nil
+}
+
+func (dbFile *File) increaseOffset(incr int64) {
+	dbFile.Offset += incr
+}
+
+// NewDBFile 创建一个DBFile
+func NewDBFile(path, filename string) (*File, error) {
+	finalPath := path + string(os.PathSeparator) + filename
+
+	file, err := os.OpenFile(finalPath, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	stat, err := os.Stat(finalPath)
+	if err != nil {
+		return nil, err
+	}
+	// Offset is zero when file create
+	return &File{Offset: stat.Size(), File: file, Path: path, FileName: filename}, nil
+}
---
+++ minidb/internal/entry.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"encoding/binary"
+)
+
+const (
+	EntryHeaderSize uint32 = 10
+)
+
+type Entry struct {
+	Key       []byte
+	Value     []byte
+	KeySize   uint32
+	ValueSize uint32
+	Mark      uint16
+}
+
+// NewEntry 创建一个Entry
+func NewEntry(key, value []byte, mark uint16) *Entry {
+	return &Entry{
+		Key:   key,
+		Value: value,
+		Mark:  mark,
+	}
+}
+
+// GetSize 获取Entry的大小
+func (e *Entry) GetSize() int64 {
+	return int64(EntryHeaderSize + e.KeySize + e.ValueSize)
+}
+
+// Encode 编码 Entry，返回字节数组
+func (e *Entry) Encode() ([]byte, error) {
+	buf := make([]byte, e.GetSize())
+	binary.BigEndian.PutUint32(buf[0:4], e.KeySize)
+	binary.BigEndian.PutUint32(buf[4:8], e.ValueSize)
+	binary.BigEndian.PutUint16(buf[8:10], e.Mark)
+	copy(buf[EntryHeaderSize:EntryHeaderSize+e.KeySize], e.Key)
+	copy(buf[EntryHeaderSize+e.KeySize:], e.Value)
+	return buf, nil
+}
+
+// Decode 解码 buf 字节数组，返回 Entry
+func Decode(buf []byte) (*Entry, error) {
+	ks := binary.BigEndian.Uint32(buf[0:4])
+	vs := binary.BigEndian.Uint32(buf[4:8])
+	mark := binary.BigEndian.Uint16(buf[8:10])
+	return &Entry{KeySize: ks, ValueSize: vs, Mark: mark}, nil
+}
