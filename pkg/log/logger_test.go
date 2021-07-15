6f948ddff332bcb3e4255542bb62da5a0a4475bb
watertreestar <watertreestar@gmail.com>
2021-07-15 21:24:39 +0800

    test: simple logger test

--- pkg/log/logger_test.go
+++ pkg/log/logger_test.go
@@ -4,5 +4,5 @@ import "testing"
 
 func TestInitLogger(t *testing.T) {
 	InitLogger()
-	Info("this is info message,username is %s","watertreestar")
+	Info("this is info message,username is %s", "watertreestar")
 }
